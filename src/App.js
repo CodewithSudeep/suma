f5971bb61cd43f4bfe16af8986505626de554ad8
SudeepMI <sudeepm0599@gmail.com>
2022-06-23 10:28:59 +0545

    voice recognization done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import "./App.css";
+// import microPhoneIcon from "./microphone.svg";
 
 function App() {
+  const { transcript, resetTranscript } = useSpeechRecognition();
+  const [isListening, setIsListening] = useState(false);
+  const microphoneRef = useRef(null);
+  
+  // stop listening after 5 seconds of silence
+  useEffect(() => {
+    if (isListening) {
+      const timeout = setTimeout(() => {
+       stopHandle();
+      }, 10000);
+      return () => clearTimeout(timeout);
+    }
+  }, [isListening]);
+
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return (
+      <div className="mircophone-container">
+        Browser is not Support Speech Recognition.
+      </div>
+    );
+  }
+  const handleListing = () => {
+    setIsListening(true);
+    microphoneRef.current.classList.add("listening");
+    SpeechRecognition.startListening({
+      continuous: false,
+    });
+  };
+  const stopHandle = () => {
+    setIsListening(false);
+    microphoneRef.current.classList.remove("listening");
+    SpeechRecognition.stopListening();
+  };
+  const handleReset = () => {
+    stopHandle();
+    resetTranscript();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="microphone-wrapper">
+      <div className="mircophone-container">
+        <div
+          className="microphone-icon-container"
+          ref={microphoneRef}
+          onClick={handleListing}
         >
-          Learn React
-        </a>
-      </header>
+          <button
+          style={{ backgroundColor: "transparent", border: "none" }}
+          >
+            🎤 
+          </button>
+        </div>
+        <div className="microphone-status">
+          {isListening ? "Listening........." : "Click to start Listening"}
+        </div>
+        {isListening && (
+          <button className="microphone-stop btn" onClick={stopHandle}>
+            Stop
+          </button>
+        )}
+      </div>
+      {transcript && (
+        <div className="microphone-result-container">
+          <div className="microphone-result-text">{transcript}</div>
+          <button className="microphone-reset btn" onClick={()=>handleReset}>
+            Reset
+          </button>
+        </div>
+      )}
     </div>
   );
 }
-
 export default App;
