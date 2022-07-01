8e20e124915c06d1c64f639c254c9cd6855ec33c
SudeepMI <sudeepm0599@gmail.com>
2022-07-01 13:16:48 +0545

    wave

---
+++ src/Wave.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './wave.css'
+
+function Wave() {
+  return (
+    <div>
+  <div class="boxContainer">
+  <div class="box box1"></div>
+  <div class="box box2"></div>
+  <div class="box box3"></div>
+  <div class="box box4"></div>
+  <div class="box box5"></div>
+</div>
+    </div>
+  )
+}
+
+export default Wave
