7dc41e5a82a838c35877b5081fe5e35eb73c1b8c
SudeepMI <sudeepm0599@gmail.com>
2022-07-02 18:17:57 +0545

    thah xaina k changes ho

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   const { transcript, resetTranscript } = useSpeechRecognition();
   const [isListening, setIsListening] = useState(false);
   const microphoneRef = useRef(null);
-
   // stop listening after 5 seconds of silence
   useEffect(() => {
     if (isListening) {
